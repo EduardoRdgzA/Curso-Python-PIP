f3696170119b5c7882b9a4794c34a0bca05eaf7b
Eduardo_Rdgz <phys.eduardo.ra@icloud.com>
2024-04-01 01:12:38 -0600

    Se comprenden las funciones world_population_percent_2, country y opulation

--- app/utils.py
+++ app/utils.py
@@ -6,12 +6,12 @@ def data_dict(ruta):
   with open(ruta,'r') as datafile:
     objeto_lector = csv.reader(datafile, delimiter = ',') #ITERADOR. Cada línea es devuelta como lista de strings ['','',...]. 
     l1_encabezado = next(objeto_lector) #LISTA. Se devuelve la primera fila. ['Rank','CCA3',...]
-    datos = [] # Lista vacía.  
+    data = [] # Lista vacía.  
     for fila in objeto_lector: # Para cada fila en el objeto lector(ITERADOR). 
       l1_zip = zip(l1_encabezado,fila) #Devuelve una lista de tuplas. [(enabezado,fila),(),...].
       l1_diccionario = {clave:valor for clave,valor in l1_zip} # Convierte la lista de tuplas a un diccionario.
       datos.append(l1_diccionario) # Se añade cada diccionario a la lista. 
-  return datos
+  return data
 # NOTA: 
 # Los iteradores están diseñados para ser recorridos secuencialmente, elemento por elemento, y una vez que un elemento ha sido consumido (leído), no puede ser accedido nuevamente sin reiniciar el iterador desde el principio
 
@@ -19,7 +19,7 @@ def data_dict(ruta):
 #{'Rank': '10', 'CCA3': 'MEX', 'Country/Territory': 'Mexico', 'Capital': 'Mexico City', 'Continent': 'North America', '2022 Population': '127504125', '2020 Population': '125998302', '2015 Population': '120149897', '2010 Population': '112532401', '2000 Population': '97873442', '1990 Population': '81720428', '1980 Population': '67705186', '1970 Population': '50289306', 'Area (km²)': '1964375', 'Density (per km²)': '64.9082', 'Growth Rate': '1.0063', 'World Population Percentage': '1.6'}
 
 
-
+'''
 #Filtro de la columna World Population Percentage obtenidos de data_dict
 def world_population_percent_1(data):
   data_population = []
@@ -27,7 +27,10 @@ def world_population_percent_1(data):
     a = linea['World Population Percentage']
     data_population.append(float(a))
   return data_population
-
+'''
+  
+# Lista de diccionarios con la estructura: [{'Country/Territory','World Population Percentage' }, ]
+# [{Argentina,.57}, {Mexico,1.6},...{Todos los países}] 
 def world_population_percent_2(data):
   data_population = []
   for diccionario in data:
@@ -37,14 +40,15 @@ def world_population_percent_2(data):
   return data_population
 
 
-
+# La siguinete función tiene como objetivo selecionar solo un país con "todos" sus datos.
+# Estructura: [{ : , : , ...}], lista de un solo diccionario.
 # Filtro por país de la data.
 def country(country,data):
   a = filter(lambda x: x['Country/Territory'] == country, data) # Filtro para seleccionar la 'country'
   return list(a) 
 
 
-#Crea un diccionario con los datos de la población del pais seleccionado
+#Crea un diccionario con los "datos de la población" del pais seleccionado.
 def population(data_country): # diccionario del país.
   country= data_country[0]
   a = {
