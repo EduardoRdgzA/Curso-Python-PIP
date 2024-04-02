d10dc8732d0b2052580b556a1edd2f9ab8191501
Eduardo_Rdgz <phys.eduardo.ra@icloud.com>
2024-04-02 16:28:03 -0600

    Update in utils.py

--- app/utils.py
+++ app/utils.py
@@ -1,8 +1,14 @@
 import csv
 import matplotlib.pyplot as plt
 
+
+###########################  LECTOR  ###########################
 #Transformación de datos. Lista de diccionarios.
-def data_dict(ruta):
+# NOTA: 
+# Los iteradores están diseñados para ser recorridos secuencialmente, elemento por elemento, y una vez que un elemento ha sido consumido (leído), no puede ser accedido nuevamente sin reiniciar el iterador desde el principio
+#Elemento de la lista (  print(data[131])):
+#{'Rank': '10', 'CCA3': 'MEX', 'Country/Territory': 'Mexico', 'Capital': 'Mexico City', 'Continent': 'North America', '2022 Population': '127504125', '2020 Population': '125998302', '2015 Population': '120149897', '2010 Population': '112532401', '2000 Population': '97873442', '1990 Population': '81720428', '1980 Population': '67705186', '1970 Population': '50289306', 'Area (km²)': '1964375', 'Density (per km²)': '64.9082', 'Growth Rate': '1.0063', 'World Population Percentage': '1.6'}
+def data_lector(ruta):
   with open(ruta,'r') as datafile:
     objeto_lector = csv.reader(datafile, delimiter = ',') #ITERADOR. Cada línea es devuelta como lista de strings ['','',...]. 
     l1_encabezado = next(objeto_lector) #LISTA. Se devuelve la primera fila. ['Rank','CCA3',...]
@@ -12,11 +18,6 @@ def data_dict(ruta):
       l1_diccionario = {clave:valor for clave,valor in l1_zip} # Convierte la lista de tuplas a un diccionario.
       data.append(l1_diccionario) # Se añade cada diccionario a la lista. 
   return data
-# NOTA: 
-# Los iteradores están diseñados para ser recorridos secuencialmente, elemento por elemento, y una vez que un elemento ha sido consumido (leído), no puede ser accedido nuevamente sin reiniciar el iterador desde el principio
-
-#Elemento de la lista (  print(data[131])):
-#{'Rank': '10', 'CCA3': 'MEX', 'Country/Territory': 'Mexico', 'Capital': 'Mexico City', 'Continent': 'North America', '2022 Population': '127504125', '2020 Population': '125998302', '2015 Population': '120149897', '2010 Population': '112532401', '2000 Population': '97873442', '1990 Population': '81720428', '1980 Population': '67705186', '1970 Population': '50289306', 'Area (km²)': '1964375', 'Density (per km²)': '64.9082', 'Growth Rate': '1.0063', 'World Population Percentage': '1.6'}
 
 
 # La siguinete función tiene como objetivo selecionar solo un país con "todos" sus datos.
@@ -29,7 +30,7 @@ def country(country,data):
 
 #Crea un diccionario con los "datos de la población" del pais seleccionado.
 def population(data_country): # diccionario del país.
-  country= data_country[0]
+  country = data_country[0]
   a = {
     int('2022') : int(country['2022 Population']),
     int('2020') : int(country['2020 Population']),
@@ -62,10 +63,10 @@ def chart(data_population,country):
   plt.savefig(f'./images/{country}.png')
   plt.close()
 
-
+###########################  EJECUCION ###########################
 #Está función solicita el país cuya gráfica se obtendrá. 
 def country_plot(path):
-  data = data_dict(path)
+  data = data_lector(path)
   pais = input('Escribe el País:')
   data_country = country(pais,data)
   data_population = population(data_country)
